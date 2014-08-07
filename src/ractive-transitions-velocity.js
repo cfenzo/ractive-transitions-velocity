f779104b064292b8f225891c45abd5a4ca76bb0c
Jens Anders Bakke <jens.anders.bakke@gmail.com>
2014-08-07 10:56:31 +0200

    Added sceleton

---
+++ src/ractive-transitions-velocity.js
@@ -0,0 +1,62 @@
+/*
+
+	ractive-transitions-velocity
+	============================
+
+	Version <%= pkg.version %>.
+
+	<< description goes here... >>
+
+	==========================
+
+	Troubleshooting: If you're using a module system in your app (AMD or
+	something more nodey) then you may need to change the paths below,
+	where it says `require( 'ractive' )` or `define([ 'ractive' ]...)`.
+
+	==========================
+
+	Usage: Include this file on your page below Ractive, e.g:
+
+	    <script src='lib/ractive.js'></script>
+	    <script src='lib/ractive-transitions-velocity.js'></script>
+
+	Or, if you're using a module loader, require this module:
+
+	    // requiring the plugin will 'activate' it - no need to use
+	    // the return value
+	    require( 'ractive-transitions-velocity' );
+
+	<< more specific instructions for this plugin go here... >>
+
+*/
+
+(function ( global, factory ) {
+
+	'use strict';
+
+	// AMD environment
+	if ( typeof define === 'function' && define.amd ) {
+		define([ 'ractive' ], factory );
+	}
+
+	// Common JS (i.e. node/browserify)
+	else if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
+		factory( require( 'ractive' ) );
+	}
+
+	// browser global
+	else if ( global.Ractive ) {
+		factory( global.Ractive );
+	}
+
+	else {
+		throw new Error( 'Could not find Ractive! It must be loaded before the ractive-transitions-velocity plugin' );
+	}
+
+}( typeof window !== 'undefined' ? window : this, function ( Ractive ) {
+
+	'use strict';
+
+	/* plugin code goes here */
+
+}));
